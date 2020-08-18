8c1c1d8799db7870cad791ce9515a0df9be47069
antoniolins <Iaab43>
2020-08-18 17:00:58 -0300

    Atualização de classe carro

--- oo/carro.py
+++ oo/carro.py
@@ -1,57 +1,73 @@
 class Motor:
-    velocidade = 0
-    def __init__(self, velocidade):
-        self.velocidade = velocidade
 
-    def acelerar_motor():
+    velocidade = None
+
+    def __init__(self, velocidade):
+        self.velocidade = 0
+
+    def acelerar(self):
         Motor.velocidade += 1
         return Motor.velocidade
 
-    def frear_motor():
+    def frear(self):
         Motor.velocidade -= 2
         if Motor.velocidade < 0:
             Motor.velocidade = 0
         return Motor.velocidade
 
-class Direção:
+class Direção(self):
     valor = 0
-    giro = ['N', 'L', 'S', 'O']
+    giro = ['Norte', 'Leste', 'Sul', 'Oeste']
 
-    def girar_a_direita():
+    print(f' giro =  {giro[(valor)]}')
+
+
+    def girar_a_direita(self):
         Direção.valor += 1
         if Direção.valor > 3:
             Direção.valor = 0
             return Direção.giro[0]
         return Direção.giro[Direção.valor]
 
-    def girar_a_esquerda():
+    def girar_a_esquerda(self):
         Direção.valor -=1
         if Direção.valor < 0:
             Direção.valor = 3
             return Direção.giro[3]
         return Direção.giro[Direção.valor]
 
-class Carro(Motor, Direção):
-    Motor.acelerar_motor()
-    print(Motor.velocidade)
+class Mycar:
+  def __init__(self, velocidade = 0, valor = 0):
+        self.velocidade = Motor.velocidade
+        self.valor = Direção.valor
+
+# carro1 = Carro()
+# carro1.acelerar_motor()
+#  print(Motor.velocidade)
+
+car1 = Mycar()
+car1.velocidade
+car1.Motor.acelerar()
+
+print(car1.velocidade)
 
 # print(Motor.velocidade)
 # print(Direção.valor)
 # print()
 # Motor.acelerar_motor()
 # Motor.acelerar_motor()
-Motor.acelerar_motor()
+#Motor.acelerar_motor()
 # Direção.girar_a_direita()
 
-print(Motor.velocidade)
+#print(Motor.velocidade)
 # print(Direção.valor)
-print()
-Motor.frear_motor()
-print(Motor.velocidade)
-print()
-Motor.frear_motor()
-print(Motor.velocidade)
-print()
+#print()
+#Motor.frear_motor()
+#print(Motor.velocidade)
+#print()
+#Motor.frear_motor()
+#print(Motor.velocidade)
+#print()
 
 # Motor.acelerar_motor()
 # print(Motor.velocidade)
