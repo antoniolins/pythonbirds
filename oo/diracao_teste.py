5f7986a254abc4a47823b999be14d0217be4510f
antoniolins <Iaab43>
2020-08-18 23:29:01 -0300

    Implementação classe carro por composição

--- oo/diracao_teste.py
+++ oo/diracao_teste.py
@@ -14,3 +14,27 @@ class Direcao:
     def girar_a_esquerda(self):
         self.__direcao = (self.__direcao - 1) % len(DIRECOES)
         self.valor = DIRECOES[self.__direcao]
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar()
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquereda()
+
