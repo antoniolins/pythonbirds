67958ea6bb0291181b591a2d316bbce4c5e9fc7b
antoniolins <Iaab43>
2020-08-18 22:34:03 -0300

    Criada classe Direcao

--- oo/carro.py
+++ oo/carro.py
@@ -1,3 +1,5 @@
+
+
 class Motor:
 
     velocidade = None
@@ -15,60 +17,29 @@ class Motor:
             Motor.velocidade = 0
         return Motor.velocidade
 
-class Direção(self):
-    valor = 0
-    giro = ['Norte', 'Leste', 'Sul', 'Oeste']
+class Direcao:
+    giros_a_direita_dct = {'Norte':  'Leste', 'Leste':  'Sul',
+                           'Sul': 'Oeste', 'Oeste': 'Norte'}
+    giros_a_esquerda_dct = {'Norte': 'Oeste', 'Oeste': 'Sul',
+                           'Sul': 'Leste', 'Leste': 'Norte'}
 
-    print(f' giro =  {giro[(valor)]}')
+    def __init__(self):
+        self.__direcao = 0
+        self.valor = giros[self.__direcao]
 
+    # print(f' giro = {giros[self.__direcao]}')
 
     def girar_a_direita(self):
-        Direção.valor += 1
-        if Direção.valor > 3:
-            Direção.valor = 0
-            return Direção.giro[0]
-        return Direção.giro[Direção.valor]
+        self.__direcao += 1
+        if self.__direcao > 3:
+            self.__direcao = 0
+            return giros[0]
+        return giros[self.__direcao]
 
     def girar_a_esquerda(self):
-        Direção.valor -=1
-        if Direção.valor < 0:
-            Direção.valor = 3
-            return Direção.giro[3]
-        return Direção.giro[Direção.valor]
-
-class Mycar:
-  def __init__(self, velocidade = 0, valor = 0):
-        self.velocidade = Motor.velocidade
-        self.valor = Direção.valor
-
-# carro1 = Carro()
-# carro1.acelerar_motor()
-#  print(Motor.velocidade)
-
-car1 = Mycar()
-car1.velocidade
-car1.Motor.acelerar()
-
-print(car1.velocidade)
-
-# print(Motor.velocidade)
-# print(Direção.valor)
-# print()
-# Motor.acelerar_motor()
-# Motor.acelerar_motor()
-#Motor.acelerar_motor()
-# Direção.girar_a_direita()
-
-#print(Motor.velocidade)
-# print(Direção.valor)
-#print()
-#Motor.frear_motor()
-#print(Motor.velocidade)
-#print()
-#Motor.frear_motor()
-#print(Motor.velocidade)
-#print()
-
-# Motor.acelerar_motor()
-# print(Motor.velocidade)
+        self.__direcao -= 1
+        if self.__direcao < 0:
+            self.__direcao = 3
+            return giro[3]
+        return giros[(self.__direcao)]
 
