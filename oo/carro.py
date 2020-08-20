9178b201f4b2ec4d80291818fe1d9d007c467cb9
antoniolins <Iaab43>
2020-08-20 00:00:39 -0300

    Implementação da Classe Carro

--- oo/carro.py
+++ oo/carro.py
@@ -29,3 +29,43 @@ class Direcao:
             self.__direcao = 3
         self.valor = GIROS[self.__direcao]
 
+class Carro:
+    def __init__(self, direcao, motor):
+        self.direcao = direcao
+        self.motor = motor
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        self.motor.acelerar() # Observe que o carro delega para o motor acelerar
+
+    def frear(self):
+        self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def calcular_direcao(self):
+        if __name__ == '__main__':
+            return self.direcao.valor
+
+    def girar_a_direita(self):
+        self.direcao.girar_a_direita()
+    def girar_a_esquerda(self):
+        self.direcao.girar_a_esquerda()
+
+direcao = Direcao()
+motor = Motor()
+
+carro = Carro(direcao, motor)
+carro.calcular_velocidade()
+carro.acelerar()
+carro.acelerar()
+print(carro.calcular_velocidade())
+carro.frear()
+print(carro.calcular_velocidade())
+carro.calcular_direcao()
+carro.girar_a_direita()
+carro.girar_a_direita()
+print(carro.calcular_direcao())
