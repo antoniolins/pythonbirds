7fd4ae07dcf77d703661ad1a0968ae22fb36d609
antoniolins <antoniolins@uol.com.br>
2020-08-24 23:21:15 -0300

    Implementação dos Testes - TesteCase()

--- oo/teste_carro.py
+++ oo/teste_carro.py
@@ -2,7 +2,6 @@ from unittest import TestCase
 from oo.carro import Motor
 
 class CarroTestCase(TestCase):
-
     def teste_velocidade_inicial(self):
         motor = Motor()
         self.assertAlmostEqual(0, motor.velocidade)
