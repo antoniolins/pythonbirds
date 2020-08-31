302e0beeea7c57236e9cc47e7f6709126d4c854e
antoniolins <Iaab43>
2020-08-31 09:59:38 -0300

    Ajuste classe calculare_pontos

--- fase.py
+++ fase.py
@@ -110,7 +110,7 @@ class Fase():
             for alvo in self._obstaculos + self._porcos:
                 passaro.colidir(alvo, self.intervalo_de_colisao)
             passaro.colidir_com_chao()
-        pontos=[self._transformar_em_ponto(a) for a in self._passaros+self._obstaculos+self._passaros]
+        pontos=[self._transformar_em_ponto(a) for a in self._passaros+self._obstaculos+self._porcos]
 
         return pontos
 
